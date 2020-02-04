ed6ae75a6ca99154ddfe653cd568b30fc9ec1e1e
chiujienwu <jchiu1@my.wctc.edu>
2020-02-03 18:40:30 -0600

    In Class Demo

---
+++ InClassStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jimmy";
+            string address = "123 Pine St";
+            var pi = Math.PI;
+
+            Console.WriteLine("Jimmy");
+            Console.WriteLine("{0}", name);
+            Console.WriteLine($"{name}");
+            Console.WriteLine("|{0}|{1}|", "Jimmy", "123 Pine St");
+            Console.WriteLine("|{0,-8}|{1,12}|", "Jimmy", "123 Pine St");
+            Console.WriteLine("|{0,-4}|{1,12}|", name, address);
+            Console.WriteLine("|{0,4}|{1,12}|", name, address);
+
+            Console.WriteLine($"{pi:n5}");
+        }
+    }
+}
